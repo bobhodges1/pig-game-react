f59f44b712e0acd1eaab3bbc0e07c48603d845f0
Bob Hodges <bobhodges1@outlook.com>
2024-04-11 22:24:35 +0100

    bad css added to die

--- src/PigGame.tsx
+++ src/PigGame.tsx
@@ -1,14 +1,24 @@
 import lodash from "lodash"
+import { useState } from "react";
 export function PigGame() {
+  const returnedArray = useState<number | null>(null)
+  const [lastRoll, setLastRoll] = returnedArray
+
 
   function rollDie() {
     const dieRoll = lodash.sample([1, 2, 3, 4, 5, 6])
     console.log("You rolled a ", dieRoll)
+    setLastRoll(dieRoll)
   }
 
-  return <div>Piggy's Game!
+  return <div>
+    <h1>Piggy's Game!</h1>
     <button onClick={rollDie}>
       Roll!
     </button>
+    <div>
+      <p>Last</p> roll was a <span className="die">{lastRoll}</span>
+    </div>
+
   </div>;
 }
