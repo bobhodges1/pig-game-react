5a161d03d5335ca21c1ece019805494dd0ef0c64
Bob Hodges <bobhodges1@outlook.com>
2024-04-11 21:57:50 +0100

    die button working

--- src/PigGame.tsx
+++ src/PigGame.tsx
@@ -1,7 +1,13 @@
+import lodash from "lodash"
 export function PigGame() {
 
+  function rollDie() {
+    const dieRoll = lodash.sample([1, 2, 3, 4, 5, 6])
+    console.log("You rolled a ", dieRoll)
+  }
+
   return <div>Piggy's Game!
-    <button>
+    <button onClick={rollDie}>
       Roll!
     </button>
   </div>;
