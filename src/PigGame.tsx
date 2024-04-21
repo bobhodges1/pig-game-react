4af623f327803f62b6c482b78d108cbc65f68ab6
Bob Hodges <bobhodges1@outlook.com>
2024-04-21 19:57:04 +0100

    added scoring

--- src/PigGame.tsx
+++ src/PigGame.tsx
@@ -1,24 +1,48 @@
 import lodash from "lodash"
 import { useState } from "react";
 export function PigGame() {
-  const returnedArray = useState<number | null>(null)
-  const [lastRoll, setLastRoll] = returnedArray
+  const [lastRoll, setLastRoll] = useState<number | null>(null)
+  const [turnScore, setTurnScore] = useState(0)
+  const [totalScore, setTotalScore] = useState(0)
 
 
   function rollDie() {
     const dieRoll = lodash.sample([1, 2, 3, 4, 5, 6])
     console.log("You rolled a ", dieRoll)
     setLastRoll(dieRoll)
+    if (dieRoll === 1) {
+      setTurnScore(0)
+    }
+    else {
+      setTurnScore(n => n + dieRoll)
+    }
+  }
+
+  function bankTurnScore() {
+    setTotalScore(totalScore + turnScore)
   }
 
   return <div>
     <h1>Piggy's Game!</h1>
+
     <button onClick={rollDie}>
       Roll!
+    </button>
+    <button onClick={bankTurnScore}>
+      Bank!
     </button>
     <div>
       Last roll was a <span className="die">{lastRoll}</span>
     </div>
+    <div>
+      Current turn score is {turnScore}
+    </div>
+    <hr />
+    <h3>Score:</h3>
+    <div>🔴 Player 1 - {totalScore}</div>
+    <br />
+    <div>🔵 Player 2 - </div>
+
 
   </div>;
 }
